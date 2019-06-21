3b0c069f8600e8876bd017347165ad26b345889d
Akosua Kernizan <akosua@Akosuas-MacBook-Air.local>
2019-06-21 16:27:50 -0400

    WIP scoreboard components

---
+++ scoreboard/app.js
@@ -0,0 +1,80 @@
+const players = {
+  {
+   name: "Akosua",
+   score: 50,
+   id:1
+  },
+  {
+   name: "Michael",
+   score: 85,
+   id:2
+  },
+  {
+   name: "Lisa",
+   score: 95,
+   id:3
+  },
+  {
+   name: "Machada",
+   score: 80,
+   id:4
+  }
+}
+
+
+const Header = (props) => {
+  return  (
+    <header>
+      <h1>{props.title}</h1>
+      <span className="stats">Players: {props.totalPlayers}</span>
+    </header>
+  );
+}
+
+const Player = (props) => {
+  return (
+    <div className="player">
+      <span className="player-name">
+        {props.name}
+      </span>
+    </div>
+
+    <Counter score={props.score}/>
+  );
+}
+
+class Counter extends React.Component {
+  render() {
+    return (
+      <div className="counter">
+        <button className="counter-action decrement"> - </button>
+        <span className="counter-score"> {props.score} </span>
+        <button className="counter-action increment"> + </button>
+      </div>
+    );
+
+  }
+
+}
+
+const App = (props) => {
+  return (
+    <div className="scoreboard">
+      <Header title="Scoreboard" totalPlayers={props.initialPlayers.length}/>
+
+      {/* Players list*/}
+      {props.initialPlayers.map( player =>
+          <Player
+            name={player.name}
+            score={player.score}
+            key={player.id.toString()}
+          />
+      )}
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <App initialPlayers={players}/>,
+  document.getElementById('root')
+);
