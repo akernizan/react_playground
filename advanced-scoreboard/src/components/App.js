4bbb283df862fec5d19eb3bd118a82f7518b4fb9
Akosua Kernizan <akosua@Akosuas-Air.home>
2019-06-23 16:29:23 -0400

    added stopwatch feature

--- advanced-scoreboard/src/components/App.js
+++ advanced-scoreboard/src/components/App.js
@@ -1,39 +1,57 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Player from './Player';
+import AddPlayerForm from './AddPlayerForm';
 
 class App extends Component {
   state = {
     players: [
       {
-        name: "Guil",
+        name: "Akosua",
         score:0,
         id: 1
       },
       {
-        name: "Treasure",
+        name: "Michael",
         score:0,
         id: 2
       },
       {
-        name: "Ashley",
+        name: "Lisa",
         score:0,
         id: 3
       },
       {
-        name: "James",
+        name: "Machada",
         score:0,
         id: 4
       }
     ]
   };
 
+  //player id Counter
+  prevPlayerId = 4;
 
   handleScoreChange = (index,delta) => {
     this.setState(prevState => {
       return{
         score: prevState.players[index].score += delta
       }
+    });
+  }
+
+  handleAddPlayer = (name) => {
+    this.setState(prevState => {
+        return {
+        ...prevState.players,
+        players: [
+            {
+              name,
+              score: 0,
+              id: this.prevPlayerId += 1,
+            },
+        ]
+      };
     });
   }
 
@@ -51,7 +69,7 @@ class App extends Component {
       <div className="scoreboard">
         <Header
           title="Scoreboard"
-          totalPlayers={this.state.players.length}
+          players={this.state.players}
         />
 
         {/* Players list */}
@@ -66,6 +84,7 @@ class App extends Component {
             removePlayer={this.handleRemovePlayer}
           />
         )}
+        <AddPlayerForm addPlayer={this.handleAddPlayer}/>
       </div>
     );
   }
